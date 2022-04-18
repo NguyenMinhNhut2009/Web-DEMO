2365affe9b8af43fd413996b5b02ef7a617f751a
NGUYỄN MINH NHỰT <4501104166@student.hcmue.edu.vn>
2022-04-18 19:21:25 +0700

    first commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,9 @@
+<div id="footer-wp">
+    <div class="wp-inner">
+        <p id="copyright">2020 © WEB TEAM</p>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ inc/footer.php
@@ -0,0 +1,139 @@
+<div id="footer-wp" style=" background: #f12a43; color: white;">
+    <div id="foot-body">
+        <div class="wp-inner clearfix">
+            <div class="block" id="info-company">
+            
+                <h3 class="title" style="color: white;">WEB TEAM</h3>
+                <p class="desc" style="color: white;">WEB TEAM luôn cung cấp luôn là sản phẩm chính hãng có thông tin rõ ràng, chính sách ưu đãi cực lớn cho khách hàng có thẻ thành viên.</p>
+                <!-- <div id="payment">
+                    <div class="thumb">
+                        <img src="public/images/img-foot.png" alt="">
+                    </div>
+                </div> -->
+            </div>
+            
+            <div class="block menu-ft" id="info-shop">
+                <h3 class="title" style="color: white;">Thông tin cửa hàng</h3>
+                <ul class="list-item">
+                    <li>
+                        <p style="color: white;">280 An Dương Vương Phường 4 Quận 5</p>
+                    </li>
+                    <li>
+                        <p style="color: white;">0908670878</p>
+                    </li>
+                    <li>
+                        <p style="color: white;">nhutxngm@mail.com</p>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="block menu-ft policy" id="info-shop">
+                <h3 class="title" style="color: white;">Chính sách mua hàng</h3>
+                <ul class="list-item">
+                    <li>
+                        <a href="" title="" style="color: white;">Quy định - chính sách</a>
+                    </li>
+                    <li>
+                        <a href="" title="" style="color: white;">Chính sách bảo hành - đổi trả</a>
+                    </li>
+                    <li>
+                        <a href="" title="" style="color: white;">Chính sách hội viện</a>
+                    </li>
+                    <li>
+                        <a href="" title="" style="color: white;">Giao hàng - lắp đặt</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+       
+    </div>
+    <div id="foot-bot">
+        <div class="wp-inner">
+            <p id="copyright" style="color: white;">Commerce © 2022 All rights reserved </p>
+            <p>Supported Email: enquiry@husol.org</p>
+        </div>
+    </div>
+</div>
+</div>
+<div id="menu-respon">
+    <a href="?page=home" title="" class="logo">WEB TEAM</a>
+    <div id="menu-respon-wp">
+        <div id="main-menu-wp">
+            <ul class="" id="main-menu-respon">
+                <li>
+                    <a href="?" title="">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="#" title="">Laptop</a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="?mod=product&act=category_product&id=1" title="">VĂN HỌC</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=2" title="">THIẾU NHI</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=3" title="">TÂM LÝ KỸ NĂNG</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=4" title="">KINH TẾ</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=5" title="">SÁCH GIÁO KHOA</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=6" title="">VĂN PHÒNG PHẨM</a>
+                        </li>
+                        <li>
+                            <a href="?mod=product&act=category_product&id=7" title="">ACER</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="?mod=post&act=blog" title="">Blog</a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="?mod=post&act=main&id=1" title="">Đánh giá</a>
+                        </li>
+                        <li>
+                            <a href="?mod=post&act=main&id=2" title="">Cẩm nang</a>
+                        </li>
+                        <li>
+                            <a href="?mod=post&act=main&id=3" title="">Tin tức</a>
+                        </li>
+                        <li>
+                            <a href="?mod=post&act=main&id=4" title="">So sánh</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="?mod=page&act=main" title="">Giới thiệu</a>
+                </li>
+                <li>
+                    <a href="?mod=page&act=contact" title="">Liên hệ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div id="btn-top"><img src="public/images/icon-to-top.png" alt=""/></div>
+<div id="fb-root"></div>
+<script>(function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id))
+            return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="198558491069654"
+  theme_color="#13cf13"
+  logged_in_greeting="Xin chào, tôi có thể hổ trợ gì cho bạn không?"
+  logged_out_greeting="Xin chào, tôi có thể hổ trợ gì cho bạn không?">
+</div>
+</body>
+</html>
