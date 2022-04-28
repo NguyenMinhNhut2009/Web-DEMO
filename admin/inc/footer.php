741f7134fa501213e7787683dd5fd5c9be0f5019
NGUYỄN MINH NHỰT <4501104166@student.hcmue.edu.vn>
2022-04-28 14:42:35 +0700

    update code demo ngày 28/1

--- admin/inc/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer-wp">
-    <div class="wp-inner">
-        <p id="copyright">Commerce © 2022 All rights reserved</p>
-    </div>
-</div>
-</div>
-</div>
-</body>
-</html>
--- admin/lib/url.php
+++
@@ -1,8 +0,0 @@
-<?php
-function redirect_to($url = '?mod=home&act=main'){
-    if(!empty($url)){
-        header("Location: {$url}");
-    }
-}
-?>
-
